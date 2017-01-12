3c350631f23dbff7531f5671fe2e559436c226e1
Lia Odette <Lia@Lias-MacBook-Pro.local>
2017-01-11 20:43:56 -0500

    replaced building class with circle building class as per the instruction of mr. puddle bear!

--- main.py
+++ main.py
@@ -1,64 +1,47 @@
 from PIL import Image, ImageDraw
 import random
-from functools import reduce
+from math import sqrt
 
 img = Image.new('RGBA', (800, 500), (0, 0, 0, 255))
 painter = ImageDraw.Draw(img)
 
 
-class Building():
+class CircleBuilding():
     
-    def __init__(self, northWest, southEast, painter):
-        self.northWest = northWest
-        self.southEast = southEast 
+    def __init__(self, center, radius, painter):
+        self.center = center
+        self.radius = radius 
         self.painter = painter
-        
-    
+
     def draw(self):
-        self.painter.rectangle(self.northWest + self.southEast, fill = (55, 255, 255, 255))
+        self.painter.ellipse((self.center[0] - self.radius, self.center[1] - self.radius,\
+                             self.center[0] + self.radius, self.center[1] + self.radius),\
+                             fill = (55, 255, 255, 255))
 
-    def north_of(self, building):
-        return self.southEast[1] <= building.northWest[1]
+    def distanceTo(self, other):
+        return sqrt(abs((other.center[0] - self.center[0]))**2 + abs((other.center[1] -\
+                    self.center[1])**2))
+    
+    def intersects(self, other):
+        return not self.distanceTo(other) >= (other.radius + self.radius)
 
-    def south_of(self, building):
-        return self.northWest[1] >= building.southEast[1]
-
-    def east_of(self, building):
-        return self.northWest[0] >= building.southEast[0]
-
-    def west_of(self, building):
-        return self.southEast[0] <= building.northWest[0]
-
-    def intersects(self, building):
-        return not (self.north_of(building) or self.south_of(building)\
-                    or self.east_of(building) or self.west_of(building)) 
-                        
 buildings = []
 
-buildingSize = (60, 60)
-
-for i in range(14):
+for i in range(10):
     while True: 
-        northWest = (random.randrange(img.size[0] - buildingSize[0]),\
-                     random.randrange(img.size[1] - buildingSize[1]))
-        southEast = (northWest[0] + buildingSize[0], northWest[1] + buildingSize[1])
-        print("I'm a random building", northWest + southEast)
-        print("I'm the buildings printed so far!", buildings)
-        building = Building(northWest, southEast, painter)
+        buildingRadius = random.randrange(70, 80)
+        buildingCenter = (random.randrange(buildingRadius, img.size[0] - buildingRadius),\
+                          random.randrange(buildingRadius, img.size[1] - buildingRadius))
+        building = CircleBuilding(buildingCenter, buildingRadius, painter)
         valid = True        
 
         for b in buildings: 
             if building.intersects(b):
                 valid = False         
 
-        print("Am I a valid building???", valid)        
         if valid == True:
             buildings.append(building)
             building.draw()
             break
 
-
-
-
 img.save('test.png')
-
