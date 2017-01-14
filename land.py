b7da0d3fa3bc008b30f7e3db264b4d0069601068
Fabrice Castel <fabrice.castel@hotmail.com>
2017-01-14 18:04:27 -0500

    scaffolding plat and land classes

---
+++ land.py
@@ -0,0 +1,3 @@
+class Land:
+    def __init__(self):
+        pass
---
+++ plat.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw
+
+class Plat:
+    def __init__(self, boundaries):
+        self.boundaries = boundaries
+
+    def draw(self, painter):
+        vertexCount = len(self.boundaries)
+
+        for vertexId in range(vertexCount):
+            start = self.boundaries[vertexId]
+            end = self.boundaries[(vertexId + 1) % vertexCount]
+            painter.line(start + end, fill = (255, 255, 255, 255)) 
